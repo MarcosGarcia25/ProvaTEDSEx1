2c14ad2e0723540ca7376bbd23a5445b6fc6437f
Marcos Paulo Marçal Garcia <marcos.mgarcia5@gmail.com>
2018-10-11 19:20:37 -0300

    Inicio do Projeto

---
+++ src/main/java/teds/PrimeiraProvaEx1/Raizes.java
@@ -0,0 +1,18 @@
+package teds.PrimeiraProvaEx1;
+
+public class Raizes {
+	
+	private double r1;
+	private double r2;
+	public Raizes(double r1, double r2) {
+		this.r1 = r1;
+		this.r2 = r2;
+	}
+	public double getR1() {
+		return r1;
+	}
+	public double getR2() {
+		return r2;
+	}
+	
+}
