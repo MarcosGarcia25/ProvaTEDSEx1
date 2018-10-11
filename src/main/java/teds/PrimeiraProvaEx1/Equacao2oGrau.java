9f47598663757ca89c89f40938ae0feb058ce227
Marcos Paulo Marçal Garcia <marcos.mgarcia5@gmail.com>
2018-10-11 19:24:51 -0300

    Criado a classe Equacao2oGrau

---
+++ src/main/java/teds/PrimeiraProvaEx1/Equacao2oGrau.java
@@ -0,0 +1,31 @@
+package teds.PrimeiraProvaEx1;
+
+public class Equacao2oGrau {
+	private double a;
+	private double b;
+	private double c;
+
+	public Equacao2oGrau(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	private double delta() {
+		return b * b - 4.0 * a * c;
+	}
+
+	private Raizes getRaizUnica() {
+		return new Raizes((-b) / (2.0 * a), (-b) / (2.0 * a));
+	}
+
+	public Raizes getRaizes() {
+		double d = delta();
+		if (d == 0.0) {
+			return getRaizUnica();
+		} else {
+			return null;
+		}
+	}
+
+}
